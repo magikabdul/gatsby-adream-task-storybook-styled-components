ded9b2e86746e9303efccd25f4243bb5ff7d8a90
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-10-29 18:50:38 +0200

    [ADG-3] added theme

---
+++ src/theme/theme.js
@@ -0,0 +1,19 @@
+const theme = {
+  color: {
+    white: '#fff',
+    black: '#000',
+    yellow: '#FFCC48',
+    almostBlack: '#040506'
+  },
+
+  font: {
+    size: {
+      s: '18px',
+      m: '28px',
+      l: '48px',
+      xl: '65px',
+    }
+  }
+}
+
+export default theme;
