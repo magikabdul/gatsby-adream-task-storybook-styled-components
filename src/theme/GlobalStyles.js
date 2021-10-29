1a678bb572d94169b94fd89b8b14286644983133
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-10-29 20:51:41 +0200

    [ADG-6] GlobalStyles moved to separate file

---
+++ src/theme/GlobalStyles.js
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components';
+import theme from '../theme/theme';
+
+const GlobalStyles = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+  }
+  
+  h1 {
+    color: ${() => theme.color.black};
+    font-size: ${() => theme.font.size.xl};
+    font-weight: bold;
+  }
+
+  h2 {
+    color: ${() => theme.color.black};
+    font-size: ${() => theme.font.size.l};
+    font-weight: bold;
+  }
+
+  h3 {
+    color: ${() => theme.color.black};
+    font-size: ${() => theme.font.size.m};
+    font-weight: bold;
+  }
+
+  h4 {
+    color: ${() => theme.color.black};
+    font-size: ${() => theme.font.size.s};
+    font-weight: bold;
+  }
+  
+  p {
+    color: ${() => theme.color.almostBlack};
+    font-size: ${() => theme.font.size.s};
+  }
+`;
+
+export default GlobalStyles;
