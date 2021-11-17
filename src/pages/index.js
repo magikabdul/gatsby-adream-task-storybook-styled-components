5d8cd2ef6309e0bc5b9dda3808c982a93c475b2d
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-11-17 23:09:11 +0100

    [ADG-22] added LocationSection

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import 'src/styles/_app.css'
 import Layout from 'src/components/layout/Layout'
 import HeroSection from 'src/components/views/HeroSection/HeroSection'
 import OfferSection from 'src/components/views/OfferSection/OfferSection'
+import LocationSection from 'src/components/views/LocationSection/LocationSection'
 
 const MainPage = () => (
   <Layout>
     <HeroSection />
     <OfferSection />
+    <LocationSection />
   </Layout>
 )
 
