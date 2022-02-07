5e72c80bdd7ae8df72532ca470cd8ec67e89a9a8
Kris <magikabdul@users.noreply.github.com>
2022-02-08 00:23:52 +0100

    [ADG-29] offer section for phones (#31)

--- src/components/molecules/Stamp/Stamp.js
+++ src/components/molecules/Stamp/Stamp.js
@@ -3,31 +3,61 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  width: 100%;
-  height: 310px;
-  background-color: ${({ theme }) => theme.color.white};
 
-  padding: 10px 0;
+  @media only screen and (min-width: 1920px) {
+    width: 100%;
+    height: 310px;
+    background-color: ${({ theme }) => theme.color.white};
+    padding: 10px 0;
+  }
 `;
 
 const IconBox = styled.div`
-  height: 136px;
-  width: 136px;
-  position: relative;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  
+  & img {
+    height: 100%;
+  }
+
+  @media only screen and (min-width: 1920px) {
+    height: 136px;
+    width: 136px;
+    position: relative;
+    display: block;
+    
+    & img {
+      position: absolute;
+      bottom: 0;
+      width: unset;
+      height: unset;
+    }
+  }
 `;
 
 const Label = styled.div`
-  margin-top: ${({ variant }) => (variant === 'high' ? '45px' : '36px')};
-  font-size: ${({ theme }) => theme.font.size.m};
-  line-height: 38px;
-  white-space: pre-wrap;
+  text-align: center;
+  font-size: ${({ theme }) => theme.font.size.xs};
+  width: 80%;
+  margin: 10px auto 0;
+
+  @media only screen and (min-width: 1920px) {
+    text-align: left;
+    margin-top: ${({ variant }) => (variant === 'high' ? '45px' : '36px')};
+    font-size: ${({ theme }) => theme.font.size.m};
+    line-height: 38px;
+    white-space: pre-wrap;
+    width: unset;
+  }
 `;
 
 const Stamp = ({ icon, label, variant }) => (
   <Container>
     <IconBox>
       <img
-        style={{ position: 'absolute', bottom: 0 }}
         src={icon}
         alt='stamp icon'
       />
