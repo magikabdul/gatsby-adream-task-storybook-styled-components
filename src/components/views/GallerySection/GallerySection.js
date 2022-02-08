e9bc830bdb5490374df44434fa267de9ae72d6df
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-02-08 21:45:07 +0100

    [ADG-34] gallery section for phones

--- src/components/views/GallerySection/GallerySection.js
+++ src/components/views/GallerySection/GallerySection.js
@@ -3,104 +3,158 @@ import styled from 'styled-components';
 import NavigationButton from 'src/components/atoms/NavigationButton/NavigationButton';
 import { StaticImage } from 'gatsby-plugin-image';
 
-const Container = styled.div`
-  position: relative;
-  margin-top: 138px;
-  width: 1920px;
-  height: 888px;
-  overflow: hidden;
+const SectionGalleryWrapper = styled.section`
+  margin: 24px 0 0 0;
+
+  @media only screen and (min-width: 1920px) {
+    position: relative;
+    margin: 138px 0 0 0;
+    width: 1920px;
+    height: 888px;
+    overflow: hidden;
+  }
 `;
 
 const Slider = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  transition: transform 0.5s ease-in-out;
-  transform: ${({ galleryShift }) => galleryShift && `translateX(-800px)`};
+
+  @media only screen and (min-width: 1920px) {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    transition: transform 0.5s ease-in-out;
+    transform: ${({ galleryShift }) => galleryShift && `translateX(-800px)`};
+  }
 `;
 
 const ButtonLeft = styled.div`
-  position: absolute;
-  width: 130px;
-  height: 130px;
-  left: 15px;
-  top: calc(50% - 65px);
+  display: none;
+
+  @media only screen and (min-width: 1920px) {
+    display: block;
+    position: absolute;
+    width: 130px;
+    height: 130px;
+    left: 15px;
+    top: calc(50% - 65px);
+  }
 `;
 
 const ButtonRight = styled.div`
-  position: absolute;
-  width: 130px;
-  height: 130px;
-  right: 15px;
-  top: calc(50% - 65px);
+  display: none;
+
+  @media only screen and (min-width: 1920px) {
+    display: block;
+    position: absolute;
+    width: 130px;
+    height: 130px;
+    right: 15px;
+    top: calc(50% - 65px);
+  }
 `;
 
 const Title = styled.h1`
-  transform: translate(1500px, 0);
+  font-size: ${({ theme }) => theme.font.size.m};
+  text-align: center;
+  margin-bottom: 24px;
+
+  @media only screen and (min-width: 1920px) {
+    font-size: ${({ theme }) => theme.font.size.xl};
+    text-align: left;
+    margin-bottom: unset;
+    transform: translate(1500px, 0);
+  }
 `;
 
 const Line = styled.div`
-  position: relative;
-  width: 450px;
-  border: ${({ theme }) => `10px solid ${theme.color.black}`};
-  transform: translate(1295px, 34px);
-  z-index: 1;
+  display: none;
+
+  @media only screen and (min-width: 1920px) {
+    display: block;
+    position: relative;
+    width: 450px;
+    border: ${({ theme }) => `10px solid ${theme.color.black}`};
+    transform: translate(1295px, 34px);
+    z-index: 1;
+  }
 `;
 
 const GalleryWrapper = styled.div`
-  transform: translate(0, -118px);
   display: grid;
-  column-gap: 30px;
-  grid-template-columns: 625px 770px 1266px;
+  row-gap: 20px;
 
-  width: 2721px;
-  height: 100%;
+  @media only screen and (min-width: 1920px) {
+    transform: translate(0, -118px);
+    column-gap: 30px;
+    grid-template-columns: 625px 770px 1266px;
+    width: 2721px;
+    height: 100%;
+  }
 `;
 
 const GalleryColumn1 = styled.div``;
 
 const GalleryColumn2 = styled.div`
   display: grid;
-  grid-template-rows: 547px 321px;
   row-gap: 20px;
+
+  @media only screen and (min-width: 1920px) {
+    grid-template-rows: 547px 321px;
+    row-gap: 20px;
+  }
 `;
 
 const GalleryColumn3 = styled.div`
   display: grid;
-  grid-template-rows: 288px 578px;
   row-gap: 20px;
+
+  @media only screen and (min-width: 1920px) {
+    grid-template-rows: 288px 578px;
+    row-gap: 20px;
+  }
 `;
 
 const GalleryRow1 = styled.div`
-  display: grid;
-  grid-template-columns: 436px 800px;
-  column-gap: 30px;
+  @media only screen and (min-width: 1920px) {
+    display: grid;
+    grid-template-columns: 436px 800px;
+    column-gap: 30px;
+  }
 `;
 
 const GalleryRow2 = styled.div`
   display: grid;
-  grid-template-columns: 800px 436px;
-  column-gap: 30px;
+  row-gap: 20px;
+  
+  @media only screen and (min-width: 1920px) {
+    grid-template-columns: 800px 436px;
+    column-gap: 30px;
+  }
 `;
 
 const ImageBox = styled.div`
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
+  width: 90%;
+  margin: 0 auto;
+  
+  @media only screen and (min-width: 1920px) {
+    width: 100%;
+    height: 100%;
+    margin: unset;
+    overflow: hidden;
 
-  & > div img {
-    transition: transform 0.3s ease-in-out !important;
-  }
+    & > div img {
+      transition: transform 0.3s ease-in-out !important;
+    }
 
-  &:hover > div img {
-    transform: scale(1.2);
+    &:hover > div img {
+      transform: scale(1.2);
+    }
   }
 `;
 
 const GallerySection = () => {
   const [galleryShift, setGalleryShift] = useState(false);
   return (
-    <Container name='gallery-section'>
+    <SectionGalleryWrapper name='gallery-section'>
       <ButtonLeft onClick={() => setGalleryShift(false)}>
         <NavigationButton direction='left' />
       </ButtonLeft>
@@ -170,7 +224,7 @@ const GallerySection = () => {
           </GalleryColumn3>
         </GalleryWrapper>
       </Slider>
-    </Container>
+    </SectionGalleryWrapper>
   );
 };
 
